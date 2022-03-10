87de70b9b9dc2e7319e3f1b5d307d2eed52f60d5
Bianca B <biancabss12@gmail.com>
2022-03-10 15:30:52 -0300

    Aula 14 - Finalizando a Home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,12 @@
 
         <!--CSS Bootstrap-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+        <script src="https://kit.fontawesome.com/332a215f17.js" crossorigin="anonymous"></script> 
 
+  
         <!--CSS da aplicação-->
         <link rel="stylesheet" href="/css/styles.css">
 
@@ -45,9 +50,68 @@
 
         {{-- Criando o footer junto também --}}
 
+<!--INICIO Footer = roda pé -->
+{{--<footer class="page-footer bg-dark">
+    <div id="contato" class="bg-success">
+        <div class="container">
+            <div class="row py-4 d-flex align-items-center">
+                <div class="col-md-12 text-center">
+                    <a href="https://pt-br.facebook.com/login/web/"><i class="fab fa-facebook-f text-white mr-4"></i></a>
+                    <a href="https://twitter.com/login?lang=pt"><i class="fab fa-twitter text-white mr-4"></i></a>
+                    <a href="https://www.instagram.com/accounts/login/?hl=pt-br"><i class="fab fa-instagram text-white mr-4"></i></a>
+                </div>
+            </div>
+        </div>   
+    </div>
+
+    <div class="container text-center text-md-left mt-5">
+        <div class="row">
+
+            <div class="col-md-3 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">Sobre nós</h6>
+                <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 125px; height: 2px">
+                <p class="mt-2 text-white">Somos um grupo de jovens que está realizando um projeto que busque reunir informações sobre o mundo game para facilitar o encontro das tendências do momento. </p>
+            </div>
+            
+            <div class="col-md-2 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">Menu</h6>
+                <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto " style="width: 125px; height: 2px">
+                <ul class="list-unstyled "> 
+                    <li class="my-2"><a href="/noticias_jogos/index.php" class="text-white">Inicio</a></li>
+                    <li class="my-2"><a href="/noticias_jogos/noticias.php" class="text-white">Notícias</a></li>
+                    <li class="my-2"><a href="/noticias_jogos/index.php" class="text-white">Vídeo</a></li>
+                    <li class="my-2"><a href="#contato" class="text-white">Contato</a></li>
+                    <li class="my-2"><a href="/noticias_jogos/src/view/login_novo/index.php" class="text-white">Produtos</a></li>
+                </ul>
+            </div>
+        
+            <div class="col-md-3 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">Contato</h6>
+                <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 125px; height: 2px">
+                <ul class="list-unstyled"> 
+                    <li class="my-2"><i class="fas fa-envelope mr-3"></i> contato@gmail.com</li>
+                    <li class="my-2"><i class="fas fa-phone mr-3"></i> (00) 0000-0000</li>
+                </ul>
+                
+                <br>
+                <h6 class="text-uppercase font-weight-bold">Configurações</h6>
+                <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 125px; height: 2px">
+                <ul class="list-unstyled"> 
+                    <li class="my-2"><a href="/noticias_jogos/src/view/login_admin/index.php" class="text-white">Administrador</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright text-center py-3">
+        <p> All Right reserved by &copy; Projeto </p>
+    </div>
+</footer>--}}
+    <!--FIM Footer = roda pé-->
+
     <footer>
         <p>HDC Events &copy; 2022</p>
     </footer>
+
     <!--Adicionando o script para os icons-->
     <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,51 @@
 {{--Chamando a função para aplicar o modelo html. Sendo aqui o INICIO--}}
 @section('content')
 
-@foreach ($events as $event) {{--chamando a tabela events na variável event--}}
-    {{--imprimindo o titulo e a descrição  --}}
-    <p>{{ $event->title }} -- {{$event->description}}</p>
-@endforeach
+<br><br><div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" name="search" id="search" class="form-control" placeholder="Procurar">
+    </form>
+    <!--
+    <div class="container">
+        <div class="d-sm-block text-right mb-4">
+            <h1 class="display-3 title-color "> Mundo Game</h1>
+            <p class="lead"> Navegue pelas fases desse mundo das notícias games!!</p>
+            <a href="#" class="btn btn-color btn-lg">Descubra</a>
+        </div>
+    </div>
+    -->
+</div>
+
+  <div id="events-container" class="col-md-12">
+      <h2>Próximos Eventos</h2>
+      <p class="subtitle">Veja os eventos dos próximos dias</p>
+     
+      <div id="cards-container" class="row">
+        @foreach ($events as $event) {{--chamando a tabela events na variável event--}}
+        <div class="card col-md-3">
+            <img src="/img/img4.jpg" alt="">
+            <div class="card-body">
+                <p class="card-date">10/03/2022</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">N Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        {{--imprimindo o titulo e a descrição  --}}
+         @endforeach
+
+      </div>
+  </div>
+
+
+
+
+
+
+
+
+
 
 
 {{--FIM da seção do conteúdo--}}
